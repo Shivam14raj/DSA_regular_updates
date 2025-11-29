cf6ac438307547565ece777933c8f82f910655dc
Shivam14raj <shivamrraj2005@gmail.com>
2025-11-29 14:57:52 +0530

    solved todays potd

---
+++ DSA/3512.cpp
@@ -0,0 +1,19 @@
+// POTD solution
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int sum=0; 
+        for(int i=0; i<nums.size(); i++){
+            sum+=nums[i];
+        } 
+        
+        int res=0; 
+        while(sum%k!=0){
+            sum=sum-1;
+            res++;
+        }
+        return res;
+
+        
+    }
+};
